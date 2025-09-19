8294b0c1bdac52238e111aa98784e075c2f090ce
Lakshmi <Lakshmi@LAPTOP-OOFKN6HD>
2025-09-19 10:31:19 +0530

    updates comitting

--- src/test/java/demopractice/PracticeGit.java
+++ src/test/java/demopractice/PracticeGit.java
@@ -1,9 +1,15 @@
 package demopractice;
 
+import org.testng.annotations.Test;
+
 public class PracticeGit {
 
+	@Test
+	
 	public void demo()
 	{
 	   System.out.println("practicing github operation");
+	   System.out.println("updating ");
+	  
 	}
 }
